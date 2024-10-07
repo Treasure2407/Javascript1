3a7228cc40b33ce17a68535d45071a4cf482f56e
Treasure2407 <johndeborah393@gmail.com>
2024-10-07 19:06:53 +0100

    first commit

---
+++ hello.js
@@ -0,0 +1,62 @@
+/*var a = prompt('a =?');
+var b =prompt('b=?');
+
+a = Number(a);
+b= Number (b);
+
+var sum = a + b;
+document.write("The sum of these two numbers is: " + sum);
+*/
+
+alert("Welcome")
+
+var myName = "My name is Debbie"; 
+document.write(myName= "My name is Debbie");
+document.write ("<br>")
+
+document.write ("<br>")
+var myName = "Basic calculations in Javascript"; 
+document.write(myName= "Basic calculations in Javascript");
+document.write ("<br>")
+
+document.write ("<br>")
+var a = "10"
+var b = "20"
+document.write(a*b);
+document.write ("<br>")
+
+document.write ("<br>")
+var c = "100"
+var d = "20"
+document.write (c/d);
+document.write ("<br>")
+
+document.write ("<br>")
+var c = "25"
+var d = "20"
+document.write (c-d);
+document.write ("<br>")
+
+document.write ("<br>")
+var c = "25"
+var d = "20"
+document.write (c + d);
+document.write ("<br>")
+
+document.write ("<br>")
+var a = "20"
+var b = "5"
+document.write (a**b);
+document.write ("<br>")
+
+document.write ("<br>")
+var c = "60"
+var d = "20"
+document.write (c/d);
+document.write ("<br>")
+
+document.write ("<br>")
+var c = "90"
+var d = "3"
+document.write (c-d);
+document.write ("<br>")
